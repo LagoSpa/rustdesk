5645413a5cb34462f25c86dd695cac79913ed0f8
lucazorzo <177193308+lucazorzo@users.noreply.github.com>
2025-11-14 11:07:47 +0100

    feat(assets): branding

--- res/gen_icon.sh
+++ res/gen_icon.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
-for size in 16 32 64 128 256 512 1024; do
-    #inkscape -z -o $size.png -w $size -h $size icon.svg >/dev/null 2>/dev/null
-    convert icon.png -resize ${size}x${size} app_icon_$size.png
+
+command -v "magick" &> /dev/null || exit 1
+command -v "inkscape" &> /dev/null || exit 1
+
+IN="${1:-"icon.svg"}"
+[[ -f "${IN}" ]] || exit 1
+
+inkscape -o icon.png -w 1024 -h 1024 ${IN} &> /dev/null
+
+sizes=""
+for size in 16 32 48 64 128 256; do
+  inkscape -o ${size}.png -w ${size} -h ${size} ${IN} &> /dev/null
+  sizes="${sizes} ${size}.png"
 done
-# from ImageMagick
-convert 16.png 32.png 48.png 128.png 256.png -colors 256 icon.ico
-#/bin/rm 16.png 32.png 48.png 128.png 256.png
+
+magick ${sizes} -colors 256 icon.ico
+
+cp 32.png "tray-icon.ico"
+
+for size in 32 64 128; do
+  mv ${size}.png "${size}x${size}.png"
+done
+mv 256.png "128x128@2x.png"
+
+rm ${sizes} &> /dev/null
+
+# for size in 16 32 64 128 256 512 1024; do
+#   inkscape -z -o ${size}.png -w ${size} -h ${size} ${IN} &> /dev/null
+# done
